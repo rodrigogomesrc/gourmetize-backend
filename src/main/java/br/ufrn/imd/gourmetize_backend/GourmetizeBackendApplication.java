6654677e866b9aa2e4df516f80a750cb13491757
Rodrigo Rocha <rocha.rodrigogomes@gmail.com>
2024-11-26 10:25:02 -0300

    cria projeto

---
+++ src/main/java/br/ufrn/imd/gourmetize_backend/GourmetizeBackendApplication.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.gourmetize_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GourmetizeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GourmetizeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufrn/imd/gourmetize_backend/GourmetizeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.gourmetize_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GourmetizeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
