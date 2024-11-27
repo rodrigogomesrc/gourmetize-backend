2f95601a8fee3f1f84517cd758d67fb57b2a5fe3
Rodrigo Rocha <rocha.rodrigogomes@gmail.com>
2024-11-26 21:56:15 -0300

    refac

--- src/main/java/br/ufrn/imd/gourmetize_backend/controller/ReceitaController.java
+++ src/main/java/br/ufrn/imd/gourmetize_backend/controller/ReceitaController.java
@@ -1,4 +1,4 @@
-package br.ufrn.imd.gourmetize_backend.control;
+package br.ufrn.imd.gourmetize_backend.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
