15db2d8afc96cce7591db8528d07191e48e89f57
Rodrigo Rocha <rocha.rodrigogomes@gmail.com>
2024-12-02 21:13:28 -0300

    adiciona auth controller

---
+++ src/main/java/br/ufrn/imd/gourmetize_backend/controller/AuthController.java
@@ -0,0 +1,22 @@
+package br.ufrn.imd.gourmetize_backend.controller;
+
+import br.ufrn.imd.gourmetize_backend.model.Usuario;
+import br.ufrn.imd.gourmetize_backend.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    private final AuthService authService;
+
+    private AuthController(@Autowired AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public Usuario login(String email, String password) {
+        return authService.login(email, password);
+    }
+}
