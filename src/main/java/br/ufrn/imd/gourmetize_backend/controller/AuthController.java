a7afbaa4d55c971c5f290a104091f9c19a5db52a
Rodrigo Rocha <rocha.rodrigogomes@gmail.com>
2024-12-02 21:19:48 -0300

    ...

--- src/main/java/br/ufrn/imd/gourmetize_backend/controller/AuthController.java
+++ src/main/java/br/ufrn/imd/gourmetize_backend/controller/AuthController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/auth")
 public class AuthController {
 
     private final AuthService authService;
