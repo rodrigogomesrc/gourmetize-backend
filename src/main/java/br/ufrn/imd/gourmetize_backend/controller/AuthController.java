227c3639acb994d6e074b5fd7e3c8b69bb5a62a9
Rodrigo Rocha <rocha.rodrigogomes@gmail.com>
2024-12-06 15:54:52 -0300

    melhora resposta de login

--- src/main/java/br/ufrn/imd/gourmetize_backend/controller/AuthController.java
+++ src/main/java/br/ufrn/imd/gourmetize_backend/controller/AuthController.java
@@ -5,6 +5,7 @@ import br.ufrn.imd.gourmetize_backend.model.dto.LoginRequest;
 import br.ufrn.imd.gourmetize_backend.service.AuthService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +22,11 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public Usuario login(@RequestBody LoginRequest body) {
-        return authService.login(body.getEmail(), body.getPassword());
+    public ResponseEntity<Usuario> login(@RequestBody LoginRequest body) {
+        try {
+            return ResponseEntity.ok(authService.login(body.getEmail(), body.getPassword()));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(401).body(null);
+        }
     }
 }
